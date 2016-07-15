411ee4f4a29763aad1d0694f76f93c69b7ece8af
rossholmberg <ross@holmberg.com.au>
2016-07-15 22:40:49 +1000

    Added "favourites" field as default in pkgLoad.

--- R/pkgLoad.R
+++ R/pkgLoad.R
@@ -6,9 +6,18 @@
 #' @keywords packages
 #' @export
 
-pkgLoad <- function( packages ) {
+pkgLoad <- function( packages = "favourites" ) {
+    
+    if( packages == "favourites" ) {
+        packages <- c( "data.table", "chron", "plyr", "dplyr", "shiny",
+                       "shinyjs", "parallel", "devtools", "doMC", "utils",
+                       "stats" )
+    }
+    
     packagecheck <- match( packages, utils::installed.packages()[,1] )
+    
     packagestoinstall <- packages[ is.na( packagecheck ) ]
+    
     if( length( packagestoinstall ) > 0L ) {
         utils::install.packages( packagestoinstall,
                                  repos = "http://cran.csiro.au"
@@ -16,9 +25,11 @@ pkgLoad <- function( packages ) {
     } else {
         print( "All requested packages already installed" )
     }
+    
     for( package in packages ) {
         suppressPackageStartupMessages(
             library( package, character.only = TRUE, quietly = TRUE )
         )
     }
+    
 }
