cda3fff22b19d9626f4e98756a030ca0ec1130fc
rossholmberg <ross@holmberg.com.au>
2016-07-15 22:40:49 +1000

    Added "favourites" field as default in pkgLoad.

--- R/pkgLoad.R
+++ R/pkgLoad.R
@@ -2,13 +2,28 @@
 #' then load that package
 #'
 #'
-#' @param packages A vector of packages to be checked or installed
+#' @param 
+#' packages A vector of packages to be checked or installed.
+#' Defaults to a list of my favourite packages, all worth having.
+#' 
 #' @keywords packages
 #' @export
 
-pkgLoad <- function( packages ) {
+pkgLoad <- function( packages = "favourites" ) {
+    
+    if( packages == "favourites" ) {
+        packages <- c( "data.table", "chron", "plyr", "dplyr", "shiny",
+                       "shinyjs", "parallel", "devtools", "doMC", "utils",
+                       "stats", "microbenchmark", "ggplot2", "readxl",
+                       "feather", "googlesheets", "readr", "DT", "knitr",
+                       "rmarkdown", "Rcpp"
+        )
+    }
+    
     packagecheck <- match( packages, utils::installed.packages()[,1] )
+    
     packagestoinstall <- packages[ is.na( packagecheck ) ]
+    
     if( length( packagestoinstall ) > 0L ) {
         utils::install.packages( packagestoinstall,
                                  repos = "http://cran.csiro.au"
@@ -16,9 +31,11 @@ pkgLoad <- function( packages ) {
     } else {
         print( "All requested packages already installed" )
     }
+    
     for( package in packages ) {
         suppressPackageStartupMessages(
             library( package, character.only = TRUE, quietly = TRUE )
         )
     }
+    
 }
