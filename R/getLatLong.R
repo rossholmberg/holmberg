76262bf269ec1d198c4bc47cf5ca0c0a0df8069e
rossholmberg <ross@holmberg.com.au>
2016-08-04 10:20:23 +1000

    Using read.table to split in one step

--- R/getLatLong.R
+++ R/getLatLong.R
@@ -10,69 +10,72 @@ getLatLong <- function( location ) {
     
     # Create an appropriate URL to search via the Google Maps API
     urlForGoogleAPI <- utils::URLencode(
-        paste0( "http://maps.googleapis.com/maps/api/geocode/json?address=", location )
+        paste0( "http://maps.googleapis.com/maps/api/geocode/xml?address=", location )
     )
     
     # attempt to retrieve data from the Google Maps API
     input <- try( 
-        utils::read.csv( urlForGoogleAPI, stringsAsFactors = FALSE ), 
+        utils::read.table( urlForGoogleAPI, 
+                           sep = ":",
+                           col.names = c( "split1", "split2" ),
+                           colClasses = "character",
+                           flush = TRUE,
+                           fill = TRUE,
+                           strip.white = TRUE,
+                           stringsAsFactors = FALSE 
+        ), 
         silent = TRUE 
     )
     
-    address <- as.character( NA )
-    lat <- as.character( NA )
-    long <- as.character( NA )
+    output <- data.frame( location = location,
+                          address = as.character( NA ),
+                          lat = as.numeric( NA),
+                          long = as.numeric( NA )
+    )
     
     # first check that the data was returned
     if( class( input ) != "try-error" ) {
-        
-        # then get to work on the data
-        names( input ) <- "incoming"
-        
-        # break up the data a little
-        breakUp <- function(x, i) { strsplit( x, " : " )[[1]][i] }
-        input$split1 <- sapply( X = input$incoming, FUN = breakUp, i = 1 )
-        input$split1 <- gsub( " ", "", input$split1 )
-        input$split2 <- sapply( X = input$incoming, FUN = breakUp, i = 2 )
         
         # see if there's a straight "location" returned from the API
         locationRow <- match( "location", input$split1 )
         
         # if so, use it (or at least the rows just below it)
         if( !is.na( locationRow ) & 
-            grepl( "lat", input$split1[ locationRow + 1 ] ) == TRUE ) {
-            lat <- gsub( " ", "", input$split2[ locationRow + 1 ] )
+            grepl( "lat", input1$split1[ locationRow + 1 ] ) ) {
+            output$lat <- as.numeric( sub( ",", "", input$split2[ locationRow + 1 ] 
+            ) )
         } else {
             # otherwise, we'll need to use the "boundary" values, using them to get a
             # best guess of the location's latitude and longitude
-            lat <- mean( as.numeric( input$split2[ grepl( "lat", input$split1 ) ] ),
-                         na.rm = TRUE
-            )
+            output$lat <- mean( as.numeric( sub( ",", "", 
+                                                 input$split2[ grepl( "lat", input$split1 ) ] 
+            ) ),
+            na.rm = TRUE )
         }
         
         # if so, use it (or at least the rows just below it)
         if( !is.na( locationRow ) & 
             grepl( "lng", input$split1[ locationRow + 2 ] ) == TRUE ) {
-            long <- gsub( " ", "", input$split2[ locationRow + 2 ] )
+            output$long <- as.numeric( sub( ",", "", input$split2[ locationRow + 2 ] 
+            ) )
         } else {
             # otherwise, we'll need to use the "boundary" values, using them to get a
             # best guess of the location's latitude and longitude
-            long <- mean( as.numeric( input$split2[ grepl( "lng", input$split1 ) ] ),
-                         na.rm = TRUE
-            )
+            output$long <- mean( as.numeric( sub( ",", "", 
+                                                  input$split2[ grepl( "lng", input$split1 ) ] 
+            ) ),
+            na.rm = TRUE )
         }
         
         
-        # see if there's a formatted address value
-        formattedAddressRow <- match( "formatted_address", input$split1 )
-        if( !is.na( formattedAddressRow ) ) {
-            address <- input$split2[ formattedAddressRow ]
+        # see if there's a formatted address value. If so, use it.
+        if( !is.na( match( "formatted_address", input$split1 ) ) ) {
+            
+            output$address <- input$split2[ match( "formatted_address", input$split1 ) ]
+            
         }
         
     }
-    
-    output <- data.frame( location = location, address = address,
-                          lat = lat, long = long )
     
     return( output )
     
