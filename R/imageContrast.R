bd094518bd013b291b73532135270227d6869bd1
rossholmberg <ross@holmberg.com.au>
2016-08-10 20:56:43 +1000

    Added the option to supply a file directly, rather than importing the array first.

--- R/imageContrast.R
+++ R/imageContrast.R
@@ -1,25 +1,37 @@
 #' calculate and apply an S curve to modify the contrast of an image
 #'
 #'
-#' @param input An array, imported from an image file.
+#' @param input A file extension to a jpg file, or an array imported from an image file.
 #' @param k Value between -1 and 1
-#' 
+#' @import jpeg
 #' @keywords image modify curve contrast
 #' @export
 
 imageContrast <- function( input, k = 0.3 ) {
     
+    # check if `input` is a file needing to be imported
+    if( class( input ) == "character" & file.exists( input ) ) {
+        
+        input <- jpeg::readJPEG( input )
+        
+    }
+    
+    # set up the curve function
     sCurve <- function( x, k ) {
         ( k * x - x ) / ( 2 * k * x - k - 1 )
     }
     
+    # create an empty array
     output <- array( data = as.numeric( NA ), dim = dim( input ) )
     
+    # apply the s curve to each side of the midpoint
     output[ input < 0.5 ] <- sCurve( x = ( 2 * input[ input < 0.5 ] ), k = k ) * 0.5
     output[ input > 0.5 ] <- sCurve( x = 2 * ( input[ input > 0.5 ] - 0.5 ), k = -k ) * 0.5 + 0.5
     
+    # fill in any values not covered yet
     output[ input == 0.5 ] <- 0.5
     
+    # return the adjusted image
     return( output )
     
 }
