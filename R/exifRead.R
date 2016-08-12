f7cd0e30b86296deced216c174b96f0d1333e8d5
rossholmberg <ross@holmberg.com.au>
2016-08-12 21:07:17 +1000

    Improved efficiency by implementing multi-core processing through `plyr` and `doMC`.

--- R/exifRead.R
+++ R/exifRead.R
@@ -3,24 +3,20 @@
 #'
 #' @param files A vector of files to be analysed.
 #' @import chron
+#' @import plyr
+#' @import doMC
 #' @keywords image exif date time name
 #' @export
 
 # files <- list.files( path = "~/Desktop/", pattern = ".jpg", full.names = TRUE )
 
-exifRead <- function( files ) {
+exifRead <- function( files, coresToUse = TRUE ) {
     
-    # create an empty data frame to be filled
-    output <- data.frame( filename = vector( mode = "character", length = length( files ) ),
-                          directory = vector( mode = "character", length = length( files ) ),
-                          date = vector( mode = "character", length = length( files ) ),
-                          time = vector( mode = "character", length = length( files ) ),
-                          stringsAsFactors = FALSE )
-    
-    for( i in seq_along( files ) ) {
+    # create a function to appropriately read information from exif data
+    getTheExifData <- function( file ) {
         
         # apply the exiftool command line tool to get the data
-        exif <- system( paste0( "exiftool ", files[i] ), intern = TRUE )
+        exif <- system( paste0( "exiftool ", file ), intern = TRUE )
         
         # separate the data, and put it into a data frame
         exif <- strsplit( exif, split = " : " )
@@ -33,21 +29,36 @@ exifRead <- function( files ) {
         datetime <- unlist( strsplit( 
             exif[ grep( "File Modification", exif$label ), 2 ], 
             split = " " ) )
-        date <- datetime[1]
-        time <- substr( datetime[2], 0, 8 )
+        date <- as.Date( datetime[1], format = "%Y:%m:%d" )
+        time <- chron::times( substr( datetime[2], 0, 8 ) )
         
         # retrieve the file details
         filename <- exif[ grep( "File Name", exif$label ), 2 ]
         directory <- exif[ grep( "Directory", exif$label ), 2 ]
         
         # add the retrieved data to the main data frame
-        output[i,] <- c( filename, directory, date, time )
+        return( data.frame( filename = filename, 
+                              directory = directory, 
+                              date = date, 
+                              time = time,
+                            stringsAsFactors = FALSE )
+        )
         
     }
     
-    # convert the dates and times to nicer formats
-    output$date <- as.Date( output$date, format = "%Y:%m:%d" )
-    output$time <- chron::times( output$time )
+    # see if we should work in parallel. Either take the number given by the user
+    if( is.numeric( coresToUse ) || is.integer( coresToUse ) ) {
+        doMC::registerDoMC( cores = coresToUse )
+        
+    # or where the user only specifies "TRUE", check for cores ourselves
+    } else if( coresToUse ) {
+        doMC::registerDoMC( cores = holmberg::whichComputer()$coresToUse )
+    }
+    
+    # apply the function to all files in the list
+    output <- plyr::ldply( .data = files, 
+                           .fun = getTheExifData,
+                           .parallel = TRUE )
     
     return( output )
     
