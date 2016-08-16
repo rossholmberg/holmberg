c2ed5f596f794de701bc71fb03dc1af684d4cc70
rossholmberg <ross@holmberg.com.au>
2016-08-16 14:53:01 +1000

    Changed to read.csv input with the `-csv` flag on exiftool. Much nicer than getting the input as a list and mucking around with that like before.

--- R/exifRead.R
+++ R/exifRead.R
@@ -14,70 +14,21 @@
 
 exifRead <- function( files, coresToUse = TRUE ) {
     
-    # create a function to appropriately read information from exif data
-    getTheExifData <- function( file ) {
-        
-        # apply the exiftool command line tool to get the data
-        exif <- system( paste0( "exiftool -s '", file, "'" ), intern = TRUE )
-        
-        # separate the data, and put it into a data frame
-        exif <- strsplit( exif, split = " : " )
-        exif <- data.frame( Reduce( rbind, exif ), 
-                            row.names = NULL, 
-                            stringsAsFactors = F )
-        names( exif ) <- c( "label", "contents" )
-        
-        # strip white space
-        exif$label <- holmberg::stripWhiteSpace( exif$label, which = "both" )
-        exif$contents <- holmberg::stripWhiteSpace( exif$contents, which = "both" )
-        
-        # retrieve the date and time
-        # first make them NA (in case they can't be found in the exif)
-        date <- as.Date( NA )
-        time <- chron::times( NA )
-        # then see if they're in the exif
-        if( "FileModifyDate" %in% exif$label ) {
-            # and make them look nice if so (overwriting the NAs we just made)
-            datetime <- unlist( strsplit( 
-                exif[ grep( "FileModifyDate", exif$label ), 2 ], 
-                split = " " ) )
-            date <- as.Date( datetime[1], format = "%Y:%m:%d" )
-            time <- chron::times( substr( datetime[2], 0, 8 ) )
-        } else {
-            # send a warning if the datetime couldn't be found
-            warning( "No datetime found, outputting as NA" )
-        }
-        
-        
-        # retrieve the file details. Starting with NA values
-        filename <- directory <- as.character( NA )
-        
-        if( "FileName" %in% exif&label ) {
-            filename <- exif[ grep( "FileName", exif$label ), 2 ]
-        } else {
-            warning( "No filename found, outputting as NA" )
-        }
-        
-        if( "Directory" %in% exif&label ) {
-            directory <- exif[ grep( "Directory", exif$label ), 2 ]
-        } else {
-            warning( "No directory found, outputting as NA" )
-        }
-        
-        # add the retrieved data to the main data frame
-        return( data.frame( filename = filename, 
-                              directory = directory, 
-                              date = date, 
-                              time = time,
-                            stringsAsFactors = FALSE )
-        )
+    # set up a basic function to retrieve the exif data
+    getTheExifData <- function( file ) { 
+        return( 
+            read.csv( text = 
+                          system( paste0( "exiftool -csv '", file, "'" ), intern = TRUE )
+            ) 
+        ) 
     }
+    
     
     # see if we should work in parallel. Either take the number given by the user
     if( is.numeric( coresToUse ) || is.integer( coresToUse ) ) {
         doMC::registerDoMC( cores = coresToUse )
         
-    # or where the user only specifies "TRUE", check for cores ourselves
+        # or where the user only specifies "TRUE", check for cores ourselves
     } else if( coresToUse ) {
         doMC::registerDoMC( cores = holmberg::whichComputer()$coresToUse )
     }
