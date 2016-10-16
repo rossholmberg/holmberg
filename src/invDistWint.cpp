1fad1f94f6770cacece040ed1495c42660b42c70
rossholmberg <ross@holmberg.com.au>
2016-10-16 19:58:12 +1100

    rejigging a little, adding proper input parameters

--- src/invDistWint.cpp
+++ src/invDistWint.cpp
@@ -2,52 +2,48 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+
 // A c++ implementation of inverse distance weighted interpolation
+
+// @param inputlat
+// @param inputlon
+// @param inputdata
+// @param outputlat
+// @param outputlon
 
 // [[Rcpp::export]]
 NumericVector invDistWInt(NumericVector inputlat,
                           NumericVector inputlon,
                           NumericVector inputdata,
                           NumericVector outputlat,
-                          NumericVector outputlon) {
+                          NumericVector outputlon ) {
     
     int noutput = outputlat.size();
-    
-    int ninput = inputlat.size();
-    
+    int ninput = inputdata.size();
     NumericVector outputdata( noutput );
-    
     NumericVector dist( ninput );
+    NumericVector inputOnDist( ninput );
     
     for(int i = 0; i < noutput; ++i) {
         
-        outputdata[i] = 99999;
+        dist = sqrt( 
+            pow( ( inputlat - outputlat[i] ), 2 ) + 
+                pow( ( inputlon - outputlon[i]) , 2 ) 
+        );
         
-        dist = sqrt( pow( (inputlat - outputlat[i]), 2 ) + pow( (inputlon - outputlon[i]), 2 ) );
-        
-        double total = 0, total_w = 0;
-        
-        for( int p = 0; p < ninput; ++p ) {
-            
-            if( dist[p] == 0 ) {
-                outputdata[i] = inputdata[p];
-                break;
-            };
-            
-            if( inputdata[p] == 99999 ) {
-                continue;
-            };
-            
-            total += inputdata[p] * ( 1 / dist[p] );
-            total_w += ( 1 / dist[p] );
-            
+        // if there's a perfectly aligned datapoint, use it (assume there's only 1)
+        if( min( dist ) == 0 ) {
+            NumericVector zeroDistData = inputdata[ dist == 0 ];
+            outputdata[ i ] = zeroDistData[ 1 ];
+            continue;
         };
         
-        if( outputdata[i] == 99999 ) {
-            outputdata[i] = total / total_w;
-        }
+        // otherwise, perform a full scale inverse distance weighted interpolation
+        outputdata[i] = sum( inputdata / dist ) / sum( 1 / dist );
+        
     }
     
     return outputdata;
     
 }
+
