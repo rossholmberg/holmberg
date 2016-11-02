cb0a81e68ac39b0a7d31ab9fb06f5c078cb45730
rossholmberg <ross@holmberg.com.au>
2016-11-02 22:22:38 +1100

    initial commit

---
+++ R/sourceFolder.R
@@ -0,0 +1,41 @@
+#' Source all files in a particular folder.
+#'
+#'
+#' @param folder Character string directory link to folder.
+#' @param except Character vector, files to avoid sourcing
+#' @param use.regex When eliminating `except` files, should we use regular expression search?
+#' @param quietly Logical, should we output running status along the way?
+#' @keywords function, load, source, script
+#' @export
+#' @return invisible character vector of files sourced
+
+sourceFolder <- function( folder, except = NULL, use.regex = FALSE, quietly = FALSE ) {
+    
+    files <- list.files( path = folder, pattern = ".R$", full.names = TRUE )
+    
+    if( !is.null( except ) && !is.na( except ) ) {
+        
+        if( use.regex ) {
+            except <- paste0( except, collapse = "|" )
+            files <- files[ !grepl( except, files ) ]
+        } else {
+            files <- files[ !( files %in% except ) ]
+        }
+        
+    }
+    
+    for( file in files ) {
+        
+        source( file )
+        
+        if( !quietly ) {
+            print( paste( "Sourced", 
+                          sapply( strsplit( file, split = "/" ), tail, n = 1L ) ) 
+            )
+        }
+        
+    }
+    
+    return( invisible( files ) )
+    
+}
