67b2a259256a039cd752a8356e556fd15e1698f4
Ross Holmberg <rholmberg@penguins.org.au>
2017-02-14 15:23:37 +1100

    adding `testDF` function

---
+++ R/testDF.R
@@ -0,0 +1,26 @@
+#' testDF, a function for creating a data frame for mucking around with.
+#'
+#' @param rows how big should the data frame be
+#' @param DT should the output be a data.table? (data.frame is output if FALSE)
+#' @importFrom data.table setDT
+#' @importFrom stats rnorm
+
+testDF <- function( rows, DT = FALSE ) {
+    
+    df <- data.frame(
+        strings = sapply( seq_len( rows ),
+                          function(x) {
+                              paste( sample( letters, 4, replace = TRUE ), collapse = "" )
+                          } ),
+        ints = sample( seq.int( from = 1000L, to = 9999L, by = 1L ), rows ),
+        rands = rnorm( rows ),
+        stringsAsFactors = FALSE
+    )
+    
+    if( DT ) {
+        setDT( df )
+    }
+    
+    return( df )
+    
+}
